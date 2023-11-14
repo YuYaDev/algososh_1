6029a2e46823558408df49baf012dec569c3e43b
Yurovskikh <yurovskikh.yasmin@yandex.ru>
2023-11-15 00:38:49 +0300

    Revert "Finish algososh"
    This reverts commit 602d68d7755ec1563e0826da55a7f9cb0e10f3b3.

--- src/components/fibonacci-page/fibonacci-page.utils.ts
+++
@@ -1,9 +0,0 @@
-export const getFibonacciNumbers = (amount: number): number[] => {
-    let arr: number[] = [1, 1];
-
-    for (let i = 2; i < amount + 1; i++) {
-        arr.push(arr[i - 2] + arr[i - 1])
-    }
-
-    return arr;
-};
